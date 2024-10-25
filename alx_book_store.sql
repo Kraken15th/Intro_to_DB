d27f6b3b964935132d904e15f1f23d6c49e680cc
Kraken15th <ahmedkarrar79@gmail.com>
2024-10-25 23:34:00 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -13,8 +13,8 @@ CREATE TABLE Authors(
 );
 CREATE TABLE Customers (
 	customer_id INT AUTO_INCREMENT PRIMARY KEY,
-	customer_name VARCHAR (215) NOT NULL,
-	email VARCHAR (215) NOT NULL,
+	customer_name VARCHAR(215) NOT NULL,
+	email VARCHAR(215) NOT NULL,
 	address TEXT
 );
 CREATE TABLE Orders (
